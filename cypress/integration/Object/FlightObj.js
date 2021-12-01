0a4686632e3eaeaaa40ddf0e92529ebbcd3904bd
binhdao112 <0932907271binh@gmail.com>
2021-12-01 15:11:32 +0700

    Update FlightObj

--- cypress/integration/Object/FlightObj.js
+++ cypress/integration/Object/FlightObj.js
@@ -1,29 +1,69 @@
+import Element_BookFlightPage from './Element/Element_BookFlightPage';
 class BookFlight{
+    constructor(){
+        this.elementBookPage=new Element_BookFlightPage()
+    }
 visitBookFlight(){
     cy.visitHome()
     cy.get('a[href="reservation.php"]').click()      
 }
-checkradio(element,value){
-    const checkbox=cy.get(element)
-    checkbox.check(value)
+checkTripTypeRadio(value){
+    this.elementBookPage.tripTypeRadio().check(value)
     return this
 }
-selection(element,value){
-    const checkbox=cy.get(element)
-    checkbox.select(value)
+selectPassCountOption(value){
+    this.elementBookPage.passCountSelection().select(value)
     return this
 }
-submit(element){
-    const button=cy.get(element)
-    button.click()
+selectFromPortOption(value){
+    this.elementBookPage.fromPortsSelection().select(value)
+    return this
 }
-verify(element,value){
-    const field=cy.get(element)
-    field.should('contain.text',value)
+selectFromMonthOption(value){
+    this.elementBookPage.fromMonthSelection().select(value)
+    return this
 }
-visible(element){
-    const field=cy.get(element)
-    field.should('be.visible')
+selectFromDayOption(value){
+    this.elementBookPage.fromDaySelection().select(value)
+    return this
 }
+
+selectTOPortOption(value){
+    this.elementBookPage.toPortsSelection().select(value)
+    return this
+}
+selectTOMonthOption(value){
+    this.elementBookPage.toMonthSelection().select(value)
+    return this
+}
+selectTODayOption(value){
+    this.elementBookPage.toDaySelection().select(value)
+    return this
+}
+checkServiceClassRadio(value){
+    this.elementBookPage.serviceClassRadio().check(value)
+    return this
+}
+selectAirLineOption(value){
+    this.elementBookPage.selection_Airline().select(value)
+    return this
+}
+submitBookFlight(){
+    this.elementBookPage.bookFlightSubmit().click()
+    return this
+}
+verifyAfterText(){
+    this.elementBookPage.afterBookText().should('contain.text',"After flight finder - No Seats Avaialble")
+    return this
+}
+verifyVisibleBackHomebtn(){
+    this.elementBookPage.backToHomebutton().should('be.visible')
+    return this
+}
+verifyVisibleBookSubmit(){
+    this.elementBookPage.bookFlightSubmit().should('be.visible')
+    return this
+}
+
 }
 export default BookFlight
