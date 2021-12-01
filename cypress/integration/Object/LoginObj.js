223430a8fbb74195c496c6d0b76e68ab2ccd4574
binhdao112 <0932907271binh@gmail.com>
2021-12-01 15:10:50 +0700

    Upodate LoginObj

--- cypress/integration/Object/LoginObj.js
+++ cypress/integration/Object/LoginObj.js
@@ -1,31 +1,59 @@
-class Login{
-visitLogin(){
-    cy.visitHome()
-    cy.get('a[href="login.php"]').click()
-}
-fill(element,value){
-    const field=cy.get(element).type(value)
-    return this
-}
-submit(element){
-    const button=cy.get(element)
-    button.click()
-}
-verify(element,value){
-    const field=cy.get(element)
-    field.should('contain.text',value)
-}
-checkemty(element){
-    const field=cy.get(element)
-    field.should('have.value','')
-}
-checkNOTemty(element){
-    const field=cy.get(element)
-    field.should('not.have.value','')
-}
-visible(element){
-    const field=cy.get(element)
-    field.should('be.visible')
-}
+import Login_Element from './Element/Element_LogIn';
+class Login {
+    constructor(){
+        this.elementpage= new Login_Element()
+    }
+    visitLogin() {
+        cy.visitHome()
+        cy.get('a[href="login.php"]').click()
+    }
+    enterUsername(value) {
+        this.elementpage.usernameInput().type(value);
+        return this
+    }
+    enterPassword(value) {
+        this.elementpage.passwordInput().type(value);
+        return this
+    }
+    submitLogin(){
+        this.elementpage.loginSubmit().click();
+        return this
+    }
+    verifyTextLoginSuccess(){
+        this.elementpage.SuccessText().should('contain.text',"Login Successfully")
+        return this
+    }
+    verifyTextLoginThank(){
+        this.elementpage.ThankText().should('contain.text',"Thank you for Loggin.")
+        return this
+    }
+    verifyTextSignOff(){
+        this.elementpage.signoffButton().should('contain.text',"SIGN-OFF")
+        return this
+    }
+    verifyEmtyUsernameInput() {
+        this.elementpage.usernameInput().should('have.value','')
+        return this
+    }
+    verifyNOTEmtyUsernameInput(element) {
+        this.elementpage.usernameInput().should('not.have.value', '')
+        return this
+    }
+    verifyEmtyPasswordInput() {
+        this.elementpage.passwordInput().should('have.value','')
+        return this
+    }
+    verifyNOTEmtyPasswordInput(element) {
+        this.elementpage.passwordInput().should('not.have.value', '')
+        return this
+    }
+    verifyVisibleLogin(){
+        this.elementpage.loginSubmit().should('be.visible')
+        return this
+    }
+    verifyTextLoginFail(){
+        this.elementpage.FailText().should('contain.text',"Enter your userName and password correct")
+        return this
+    }
 }
 export default Login
