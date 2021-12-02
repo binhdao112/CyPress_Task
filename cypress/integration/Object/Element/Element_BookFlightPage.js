2c9539480d6034d94ff283b8c9e7fdbba9b64d16
binhdao112 <0932907271binh@gmail.com>
2021-12-02 11:30:24 +0700

    Update Element_BookFlightPage

--- cypress/integration/Object/Element/Element_BookFlightPage.js
+++ cypress/integration/Object/Element/Element_BookFlightPage.js
@@ -1,42 +1,42 @@
-class Element_BookFlightPage{
-    tripTypeRadio(){
+class Element_BookFlightPage {
+    tripTypeRadio() {
         return cy.get("input[name='tripType']")
     }
-     passCountSelection(){
-         return cy.get("select[name='passCount'")
-     }
-     fromPortsSelection(){
-         return cy.get("select[name='fromPort']")
-     }
-     fromMonthSelection(){
-         return cy.get("select[name='fromMonth']")
-     }
-     fromDaySelection(){
-         return cy.get("select[name='fromDay']")
-     }
-     toPortsSelection(){
+    passCountSelection() {
+        return cy.get("select[name='passCount'")
+    }
+    fromPortsSelection() {
+        return cy.get("select[name='fromPort']")
+    }
+    fromMonthSelection() {
+        return cy.get("select[name='fromMonth']")
+    }
+    fromDaySelection() {
+        return cy.get("select[name='fromDay']")
+    }
+    toPortsSelection() {
         return cy.get("select[name='toPort']")
     }
-    toMonthSelection(){
+    toMonthSelection() {
         return cy.get("select[name='toMonth']")
     }
-    toDaySelection(){
+    toDaySelection() {
         return cy.get("select[name='toDay']")
     }
-    serviceClassRadio(){
+    serviceClassRadio() {
         return cy.get("input[name='servClass']")
     }
-    selection_Airline(){
-         return cy.get("select[name='airline'")
-     }
-     bookFlightSubmit(){
-         return cy.get("input[name='findFlights'][type='image']")
-     }
-     afterBookText(){
-         return cy.get('font')
-     }
-     backToHomebutton(){
-         return cy.get('td > a > img[src="images/home.gif"]')
-     }
+    selection_Airline() {
+        return cy.get("select[name='airline'")
+    }
+    bookFlightSubmit() {
+        return cy.get("input[name='findFlights'][type='image']")
+    }
+    afterBookText() {
+        return cy.get('font')
+    }
+    backToHomebutton() {
+        return cy.get('td > a > img[src="images/home.gif"]')
+    }
 }
 export default Element_BookFlightPage
